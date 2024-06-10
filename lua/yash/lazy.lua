55dd535d8080bbb70bed0f4775af2bf3bb3aa3c6
Yash Khatri <yashXkhatri@proton.me>
2024-06-10 10:17:23 +0530

    added lsp

--- lua/yash/lazy.lua
+++ lua/yash/lazy.lua
@@ -11,7 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("yash.plugins", {
+require("lazy").setup({ { import = "yash.plugins" }, { import = "yash.plugins.lsp" } }, {
   checker = {
     enable = true,
     notify = false,
